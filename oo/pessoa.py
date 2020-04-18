cf5ae9af147a33fd398d112cbacbd5cfd97bdb57
k <daanrod93@gmail.com>
2020-04-18 00:02:12 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
+    # atributo de classe default
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=27):
         self.idade = idade
         self.nome = nome
@@ -18,7 +21,18 @@ if __name__ == '__main__':
     print(danilo.idade)
     for filho in danilo.filhos:
         print(filho.nome)
+    # atributos podem ser criados dinamicamente, mas é recomendados apenas para
+    # eventialidades
     danilo.sobrenome = "Barros"
+    # atributos podem ser deletados dinamicamente
     del danilo.filhos
+    danilo.olhos = 1
+    del danilo.olhos
     print(diego.__dict__)
     print(danilo.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(diego.olhos)
+    print(danilo.olhos)
+    print(id(Pessoa.olhos), id(danilo.olhos), id(diego.olhos))
+
